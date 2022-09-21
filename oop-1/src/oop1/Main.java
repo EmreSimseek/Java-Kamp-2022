be2c2ec146d7ded03083e781875d5fd38af4cd34
Emre Şimşek <emre0816s@gmail.com>
2022-09-21 21:48:12 +0300

    Update Main.java

--- oop-1/src/oop1/Main.java
+++ oop-1/src/oop1/Main.java
@@ -3,6 +3,10 @@ package oop1;
 public class Main {
 
     public static void main(String[] args) {
+        String mesaj ="Vade oranı";
+
+        Product product1 = new Product(); //Class tanımlama
+        product1.name="Delonghi Kahve Makinesi";
 
     }
 }
