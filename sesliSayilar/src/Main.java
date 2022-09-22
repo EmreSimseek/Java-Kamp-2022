b0eb5eff493247c366a2169c2e1cd67f01473673
Emre Şimşek <emre0816s@gmail.com>
2022-09-22 23:59:22 +0300

    week1

---
+++ sesliSayilar/src/Main.java
@@ -0,0 +1,25 @@
+public class Main {
+
+
+    public static void main(String[] args) {
+
+        char harf ='E';
+
+        switch (harf)
+        {
+            case'A':
+            case'I':
+            case'U':
+            case'O':
+                System.out.println("Kalin sesli harf");
+                break;
+            default:
+                System.out.println("Ince sesli harf");
+
+
+        }
+    }
+}
+
+
+
