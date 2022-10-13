a413bd483dccc9ce3bedf6685f44346bca5ce49e
Emre Şimşek <emre0816s@gmail.com>
2022-10-13 14:57:15 +0300

    week-3

---
+++ abstractClasses/src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        WomanGameCalculator womanGameCalculator = new WomanGameCalculator();
+        womanGameCalculator.hesapla();
+        womanGameCalculator.gameOver();
+
+        GameCalculator gameCalculators =new WomanGameCalculator();
+
+
+
+    }
+}
