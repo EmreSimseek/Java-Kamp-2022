10ccd089b2be46d08d4c1dd061d4a169667c79d3
Emre Şimşek <emre0816s@gmail.com>
2022-10-13 12:11:28 +0300

    week-3

---
+++ polmyorphismDemo/src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+
+    public static void main(String[] args) {
+   /*  BaseLogger[] loggers = new BaseLogger[]{new FileLogger(),new EmailLogger(),new DataBaseLogger()};
+     for(BaseLogger logger:loggers) // tüm hedeflere loglama işlemi
+     {
+         logger.Log("Log mesajı");
+     }
+
+    } */
+        CustomerManager customerManager = new CustomerManager(new DataBaseLogger());
+        customerManager.add();
+    }
+}
